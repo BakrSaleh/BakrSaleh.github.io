e042de06bbdb2916765dead0342930ba91e0ab15
Period 2 <bakrsaleh@students.berkeley.net>
2020-03-12 09:46:25 -0700

    Initial commit

---
+++ sketch.js
@@ -0,0 +1,18 @@
+var x = 0;
+
+function setup() {
+  createCanvas(600, 400);
+}
+
+function draw () {
+  background(0);
+  stroke(255);
+  strokeWeight(4);
+  noFill();
+  ellipse(x, 200, 100, 100);
+  
+  if (x > width) {
+  x = x - 3;
+  }
+  x = x + 3;
+}
